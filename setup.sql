5ab541d414f07f3287b32a795861a8b92e6ea145
DeVonte G <dagray92@hotmail.com>
2022-08-08 23:45:03 -0400

    vehicle crud start

---
+++ setup.sql
@@ -0,0 +1,55 @@
+CREATE TABLE vehicle (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    make TEXT,
+    model TEXT,
+    owner_first VARCHAR(30),
+    owner_last VARCHAR(30)
+);
+
+INSERT INTO vehicle (
+    make,
+    model,
+    owner_first,
+    owner_last
+) Values (
+    "Honda",
+    "Accord",
+    "DeVonte",
+    "Gray"
+);
+
+INSERT INTO vehicle (
+    make,
+    model,
+    owner_first,
+    owner_last
+) Values (
+    "Ford",
+    "Mustang",
+    "Jason",
+    "Bourne"
+);
+
+INSERT INTO vehicle (
+    make,
+    model,
+    owner_first,
+    owner_last
+) Values (
+    "Honda",
+    "Accord",
+    "John",
+    "Wick"
+);
+
+INSERT INTO vehicle (
+    make,
+    model,
+    owner_first,
+    owner_last
+) Values (
+    "Chevy",
+    "Impala",
+    "Keanu",
+    "Reeves"
+);
