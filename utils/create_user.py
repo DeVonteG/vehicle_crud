6caf415482ea1174b489098f5b6460f4e3326cb0
DeVonte G <dagray92@hotmail.com>
2022-08-09 03:45:00 -0400

    vehicle crud updated

---
+++ utils/create_user.py
@@ -0,0 +1,28 @@
+import requests
+
+URL = "http:127.0.0.1:5002/vehicles"
+
+def create_vehicle(make, model, owner_first, owner_last):
+    owner={
+        "make": make,
+        "model": model,
+        "owner_first": owner_first,
+        "owner_last": owner_last,
+    }
+    response = requests.post(URL, json=owner)
+    if response.status_code == 201:
+        print(
+            "Successfully created a new user; Got: %s"
+            % response.json().get("message")
+        )
+    else:
+        print(
+            "Something went wrong while creating a new user."
+        )
+
+if __name__ == "__main__":
+    vmake = input("Type in make of your vehicle: ")
+    vmodel = input("Type in your model of your vehicle: ")
+    fname = input("Type in users first name: ")
+    lname = input("Type in users last name: ")
+    create_vehicle(vmake, vmodel, fname, lname)
